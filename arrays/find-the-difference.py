64f7317103462f2d7bac58ac2c46be891f230347
Mortoti <mortoti.dev@gmail.com>
2025-09-24 10:44:38 +0000

    Day 2: Strings - Find the Difference Solution

---
+++ arrays/find-the-difference.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        # XOR all chars in s and t, pairs cancel out, leaving the extra char
+
+        result = 0
+        for letter in t + s:
+                result ^= ord(letter)   
+        return chr(result)
+
+        
+solution = Solution()
+print(solution.findTheDifference("abcd", "abcde"))
+
+        
